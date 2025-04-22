30f5dd723363f476e0ce8505dc1102b167979e68
AdamRB208 <adamrborup@gmail.com>
2025-04-21 22:39:23 -0600

    rendering blog pojos

--- src/services/BlogService.js
+++ src/services/BlogService.js
@@ -1,10 +1,15 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Blog } from "@/models/Blogs.js"
+import { AppState } from "@/AppState.js"
 
 class BlogService {
   async getBlogs() {
     const response = await api.get('api/blogs')
     logger.log('got blogs', response.data)
+    const blogs = response.data.map(pojo => new Blog(pojo))
+    AppState.blogs = blogs
+    logger.log('rendering blogs', AppState.blogs)
   }
 
 }
