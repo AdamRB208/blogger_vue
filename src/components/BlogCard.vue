f56c99db6487e70d5a552362401aa3ccac60792c
AdamRB208 <adamrborup@gmail.com>
2025-04-21 23:11:12 -0600

    created page for blog card

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Blog } from '@/models/Blogs.js';
+
+
+defineProps({
+  blogProp: { type: Blog, required: true }
+})
+</script>
+
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
