77b3d1f2eca8c3f4d3dbf93bf939adc5493454b3
AdamRB208 <adamrborup@gmail.com>
2025-04-24 16:10:23 -0600

    styled blog post space.

--- src/pages/BlogDetailsPage.vue
+++ src/pages/BlogDetailsPage.vue
@@ -32,29 +32,34 @@ async function getBlogByBlogId() {
 <template>
   <section class="container" v-if="blog">
     <div class="row justify-content-center">
-      <div class="col-md-10">
-        <div class="mt-5 mb-3 border border-3 border-custom-purple rounded-4 d-flex flex-column align-items-center">
+      <div class="col-md-10 border border-3 border-custom-purple rounded-4 mt-5">
+        <div class="mt-3 mb-3 d-flex flex-column align-items-center">
           <div>
             <img
               :src="blog.imgUrl || 'https://media.istockphoto.com/id/1573249349/photo/cat-face-meme.webp?a=1&b=1&s=612x612&w=0&k=20&c=kqbadSpx9y1sUvUjbO-zTr4iRDv2inL5XfOhts5-jGs='"
               alt="" class="pt-2 rounded-4">
           </div>
+        </div>
+        <div class="d-flex flex-row justify-content-evenly">
+          <div>
+            <img :src="blog.creator.picture" alt="" class="d-flex flex-start creator-img">
+          </div>
           <div class="mt-3 mb-3 d-flex justify-content-center align-items-center">
-            <div class="d-flex">
-              <img :src="blog.creator.picture" alt="" class="creator-img">
-            </div>
             <span class="ms-3">
               <h1>{{ blog.title }}</h1>
               <small class="d-flex flex-column ms-0">{{ blog.creator.name }}</small>
               <small>Created on {{ blog.createdAt.toLocaleDateString() }}</small>
             </span>
           </div>
+        </div>
+        <div>
           <div>
             <p class="d-block fs-3 p-3">{{ blog.body }}</p>
           </div>
         </div>
       </div>
     </div>
+
   </section>
   <p v-else>Loading</p>
 </template>
